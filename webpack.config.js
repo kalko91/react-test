19fb8627e0b24b91f7f0e944a8db427279530e50
Yakovlev Sergey <kolko91@gmail.com>
2017-09-21 15:03:03 +0300

    fixed untracked files

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,29 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: __dirname + '/src/index.html',
+    filename: 'index.html',
+    inject: 'body'
+});
 module.exports = {
-  devtool: 'cheap-module-eval-source-map',
-  entry: [
-    'webpack-hot-middleware/client',
-    './src/index'
-  ],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/static/'
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['react-hot', 'babel'],
-      include: path.join(__dirname, 'src')
-    }]
-  }
+    entry: './src/app.js',
+    output: {
+        path: path.join(__dirname,'dist'),
+        filename: 'app.bundle.js'
+    },
+    module: {
+        loaders: [
+            {test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/},
+            {test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/}
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname,'dist'),
+        compress: true,
+        host:'0.0.0.0',
+        port: 9000,
+        open: true
+    },
+    plugins: [HtmlWebpackPluginConfig]
 };
