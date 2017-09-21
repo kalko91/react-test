f6f039570462f9b058ea898adde81ade492041da
Valerie V <heyfilky@gmail.com>
2017-09-21 16:37:23 +0300

    + html

--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -5,13 +5,16 @@ import { Component } from 'react';
 export class UserCard extends Component {
     render() {
         return (
-            <div>
-                John Smith
-                Father
-                email@gmail.com
-                +52181801214563464
-
-
+            <div class="card">
+                <p class="card__name">John Smith</p>
+                <p class="card__title">Father</p>
+                <div class="card__wrapper">
+                    <p class="card__email">email@gmail.com</p>
+                    <p class="card__phone">+52181801214563464</p>
+                </div>
+                <button class="btn bnt_edit"></button>
+                <button class="btn bnt_del"></button>
+                <button class="btn bnt_next"></button>
             </div>
         );
     }
